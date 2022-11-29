1a1db9fb23fb2fff939c019db39d25fe540e857d
Jia Sheng <shengjia1100@gmail.com>
2022-11-29 21:50:29 +0100

    add ex.3.12 mongo.js

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.kytfqgm.mongodb.net/?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+    if (process.argv.length == 3) {
+      Person
+        .find({})
+        .then(result => {
+          result.forEach(person => {
+            console.log(person)
+          })
+        mongoose.connection.close()
+        })
+    }
+    else if (process.argv.length == 5)
+    {
+      const name_ = process.argv[3]
+      const number_ = process.argv[4]
+      const person = new Person({
+        name: `${name_}`,
+        number: `${number_}`,
+      })
+      person.save().then(result => {
+        console.log(`added ${name_} number ${number_} to phonebook`)
+        mongoose.connection.close()
+      })
+    }
+  })
+  .catch((err) => console.log(err))
